6bca01005a998935e68397064fde69fbc2fb81d8
TibebesJs <Tibebes.js@gmail.com>
2020-04-25 11:27:56 -0400

    create a sample right sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,6 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      :mini-variant="drawer"
-      clipped
-      fixed
-      app
-    >
+    <v-navigation-drawer :mini-variant="drawer" clipped fixed app>
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -23,19 +18,12 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      clipped-left
-      fixed
-      app
-    >
+    <v-app-bar clipped-left fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
     <v-content>
@@ -45,25 +33,55 @@
     </v-content>
     <v-navigation-drawer
       v-model="rightDrawer"
-      :right="right"
+      right
       temporary
       fixed
+      class="d-flex"
     >
       <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        <v-list-item @click.native="right = !right" class="pb-3">
+          <v-list-tile-avatar class="pr-2">
+            <v-avatar
+              color="accent"
+            >
+            <img
+              src="https://habeshaflix.com/images/artists/a2.jpg"
+              width="55"
+            />
+            </v-avatar>
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-item-title class="pl-1">
+              Selam Tesfaye
+            </v-list-item-title>
+            <VListTileSubTitle>
+              <v-btn color="primary" text @click="alert" x-small class="pa-0 px-1">
+                Profile
+              </v-btn>
+              <v-btn color="primary" text @click="alert" x-small class="pa-0 px-1">
+                Sign out
+              </v-btn>
+            </VListTileSubTitle>
+          </v-list-tile-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item>
+          <v-btn-toggle v-model="text" tile color="accent-3" group>
+            <v-btn value="left">
+              English
+            </v-btn>
+
+            <v-btn value="center">
+              አማርኛ
+            </v-btn>
+          </v-btn-toggle>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
+    <v-footer :fixed="fixed" app>
       <span>&copy; {{ new Date().getFullYear() }} GAST Entertainemnt Mall</span>
     </v-footer>
   </v-app>
@@ -71,7 +89,7 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       drawer: false,
       items: [
@@ -86,8 +104,7 @@ export default {
           to: '/inspire'
         }
       ],
-      right: true,
-      rightDrawer: false,
+      rightDrawer: true,
       title: 'Gast Cinema Admin Panel'
     }
   }

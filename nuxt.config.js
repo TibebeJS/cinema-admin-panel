3dc2b6018f5f9509177cf0619f6ad49a43cbdfd5
TibebesJs <Tibebes.js@gmail.com>
2020-04-25 10:21:45 -0400

    change primary color to cyan

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,7 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
+          primary: colors.cyan.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,

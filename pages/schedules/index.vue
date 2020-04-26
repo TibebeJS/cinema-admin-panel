65970a2d4a9c90d0856fe96c589963f4c30a8299
TibebesJs <Tibebes.js@gmail.com>
2020-04-26 04:10:38 -0400

    make simple layout adjustments

--- pages/schedules/index.vue
+++ pages/schedules/index.vue
@@ -1,78 +1,146 @@
 <template>
-  <v-card width="100%">
-    <v-card-title class="headline">
-      Schedules
-      <v-spacer></v-spacer>
-      <v-btn>New Schedule</v-btn>
-    </v-card-title>
-    <v-card-subtitle>
-      Mange cinema schedules
-    </v-card-subtitle>
-    <v-card-text>
-      <v-expansion-panels>
-        <v-expansion-panel>
-          <v-expansion-panel-header>
-            <v-row class="align-start align-baseline">
-              <v-icon>mdi-filter</v-icon> Filters :
-              <span class="secondary--text">{{ picker.slice(0, 10) }}</span>
+  <v-container>
+    <v-card width="100%">
+      <v-card-title class="headline">
+        Schedules
+        <v-spacer></v-spacer>
+        <v-btn>New Schedule</v-btn>
+      </v-card-title>
+      <v-card-subtitle>
+        Mange cinema schedules
+      </v-card-subtitle>
+      <v-card-text>
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              <v-row class="align-start align-baseline">
+                <v-icon>mdi-filter</v-icon> Filters :
+                <span class="secondary--text">{{ picker.slice(0, 10) }}</span>
 
-              <v-col
-                v-for="(selection, i) in selections"
-                :key="selection.text"
-                class="shrink"
+                <v-col
+                  v-for="(selection, i) in selections"
+                  :key="selection.text"
+                  class="shrink"
+                >
+                  <v-chip
+                    :disabled="loading"
+                    close
+                    @click:close="selected.splice(i, 1)"
+                    small
+                  >
+                    {{ selection.text }}
+                  </v-chip>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-row>
+                <v-col>
+                  <v-date-picker
+                    v-model="picker"
+                    landscape
+                    type="month"
+                    no-title
+                    reactive
+                  ></v-date-picker>
+                  <v-date-picker
+                    v-model="picker"
+                    landscape
+                    no-title
+                    reactive
+                  ></v-date-picker>
+                </v-col>
+                <v-col>
+                  <v-list>
+                    <span class="subtitle-1">
+                      Movie Filters:
+                    </span>
+                    <template v-for="(item, i) in categories">
+                      <v-list-item
+                        :class="{ 'active-filter': selected.includes(i) }"
+                        class="my-2"
+                        :key="i"
+                        :disabled="selected.includes(i)"
+                        @click="selected.push(i)"
+                      >
+                        <v-list-item-title
+                          v-text="item.text"
+                        ></v-list-item-title>
+                      </v-list-item>
+                    </template>
+                  </v-list>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-card-text>
+    </v-card>
+
+    <v-card width="100%" class="mt-5">
+     
+      <v-card-text>
+        <v-layout>
+          <v-card
+            class="mx-auto my-12 px-2"
+            max-width="374"
+            v-for="schedule of schedules"
+            :key="schedule"
+          >
+            <v-img
+              height="250"
+              :src="`http://image.tmdb.org/t/p/w185/${schedule.poster_path}`"
+            ></v-img>
+
+            <v-card-title>{{ schedule.title }}</v-card-title>
+
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                Ratings:
+                <div class="grey--text ml-1">{{ schedule.vote }}</div>
+              </v-row>
+
+              <div>{{ schedule.overview.slice(0, 100) }} ...</div>
+            </v-card-text>
+
+            <v-divider class="mx-4"></v-divider>
+
+            <v-card-title>Recorded Schedules</v-card-title>
+
+            <v-card-text>
+              <v-chip-group
+                v-model="selection"
+                active-class="deep-purple accent-4 white--text"
+                column
               >
                 <v-chip
-                  :disabled="loading"
+                  class="ma-2"
                   close
-                  @click:close="selected.splice(i, 1)"
-                  small
+                  text-color="white"
+                  close-icon="mdi-delete"
+                  v-for="movieSchedule in schedule.schedules"
+                  :key="movieSchedule.id"
+                  @click:close="close"
                 >
-                  {{ selection.text }}
+                  <v-avatar left>
+                    2D
+                  </v-avatar>
+                  {{ movieSchedule.Cinema.name }} |
+                  {{ movieSchedule.time.slice(0, 5) }}
                 </v-chip>
-              </v-col>
-            </v-row>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-row>
-              <v-col>
-                <v-date-picker
-                  v-model="picker"
-                  landscape
-                  type="month"
-                  no-title
-                  reactive
-                ></v-date-picker>
-                <v-date-picker
-                  v-model="picker"
-                  landscape
-                  no-title
-                  reactive
-                ></v-date-picker>
-              </v-col>
-              <v-col>
-                <v-list>
-                  <span class="subtitle-1">
-                    Movie Filters:
-                  </span>
-                  <template v-for="(item, i) in categories">
-                    <v-list-item
-                      :class="{ 'active-filter': selected.includes(i) }"
-                      class="my-2"
-                      :key="i"
-                      :disabled="selected.includes(i)"
-                      @click="selected.push(i)"
-                    >
-                      <v-list-item-title v-text="item.text"></v-list-item-title>
-                    </v-list-item>
-                  </template>
-                </v-list>
-              </v-col>
-            </v-row>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </v-card-text>
-  </v-card>
+              </v-chip-group>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn text color="error lighten-2" outlined>
+                remove all
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-layout>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 <style scoped>
 .active-filter {

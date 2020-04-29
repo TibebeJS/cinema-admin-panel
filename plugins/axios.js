f61ad75cd1051768115a481afc31a2967fb2e270
TibebesJs <Tibebes.js@gmail.com>
2020-04-29 15:27:18 -0400

    fix some bug on error handler on interceptor

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,11 +1,18 @@
 const { auth } = require('../services/fireinit.js')
 
-export default function ({ $axios, redirect }) {
-  $axios.onError(async error => {
-    if(error.response.status === 401) {
-      $axios.setToken(auth.currentUser.getIdToken(), 'Bearer')
-
-      return $axios(error.config);
+export default function ({ $axios, redirect, $store }) {
+  $axios.onError(error => {
+    if (error.response.status === 401) {
+      return new Promise((resolve, reject) => {
+        $axios.setToken(auth.currentUser.getIdToken(), 'Bearer')
+        $axios(error.config).then(data => {
+          resolve(data)
+        }).reject((err) => {
+          $store
+          .dispatch('signOut')
+          .then(() => redirect({ name: 'auth' }))
+        });  
+      })
     }
   })
 }

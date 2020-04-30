abf88fa9b15be57a622615d722a6ed28983b5e34
TibebesJs <Tibebes.js@gmail.com>
2020-04-30 15:00:58 -0400

    some interceptor code change

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,8 @@ const { auth } = require('../services/fireinit.js')
 
 export default function({ $axios, redirect, $store }) {
   $axios.onError(error => {
-    if (error.response.status === 401) {
+    const code = parseInt(error.response && error.response.status)
+    if (code === 401) {
       return new Promise((resolve, reject) => {
         $axios.setToken(auth.currentUser.getIdToken(), 'Bearer')
         $axios(error.config)
